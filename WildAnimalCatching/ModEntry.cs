46de7377ac3435ad007fe2406ba67289190fe920
Joe Sogard <jmsogard@gmail.com>
2019-02-07 22:13:12 -0600

    initial commit

---
+++ WildAnimalCatching/ModEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+
+namespace WildAnimalCatching
+{
+    class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
